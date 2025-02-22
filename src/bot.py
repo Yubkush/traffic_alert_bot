d4794fbadd1f4951e57b1cd3c082dc610f21f685
Yuval Lande <yuvalande@gmail.com>
2025-02-22 17:32:34 +0200

    Changing bot to send game data

--- src/bot.py
+++ src/bot.py
@@ -1,19 +1,38 @@
 import os
+from datetime import time
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, ContextTypes, JobQueue
+
+from web_parser import get_week_game_data, get_next_day_game_data
+
+
+GROUP_ID = "@mtm_trafic_alert_grp"
 
 load_dotenv()
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+def to_utc(hour: int):
+    return hour - 2
+
+
+async def send_games_weekly(context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(chat_id=GROUP_ID, text=get_week_game_data())
+
+
+async def send_games_daily(context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(chat_id=GROUP_ID, text=get_next_day_game_data())
+
 
 def main() -> None:
-    app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+    """Run bot that sends game data"""
+    app = ApplicationBuilder().token(os.getenv("TOKEN")).job_queue(JobQueue()).build()
 
-    app.add_handler(CommandHandler("hello", hello))
+    app.job_queue.run_daily(send_games_weekly, time(to_utc(20)), days=(6,))
+
+    app.job_queue.run_daily(send_games_daily, time(to_utc(21)), days=(0, 1, 2, 3, 6))
 
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
--- src/web_parser.py
+++ src/web_parser.py
@@ -1,32 +1,40 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.haifa-stadium.co.il/%d7%9c%d7%95%d7%97_%d7%94%d7%9e%d7%a9%d7%97%d7%a7%d7%99%d7%9d_%d7%91%d7%90%d7%a6%d7%98%d7%93%d7%99%d7%95%d7%9f/"
 WEEKDAYS = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
 ]
 
-def get_week_day_str(date_str: datetime.date):
+
+def get_week_day_str(date_str):
     return WEEKDAYS[date_str.weekday()]
+
 
 def is_date(date_string: str):
     try:
-        datetime.strptime(date_string, '%d/%m/%Y')
+        datetime.strptime(date_string, "%d/%m/%Y")
         return True
     except ValueError:
         return False
 
+
 def is_game_info(strs: list[str]):
     return len(strs) == 3
 
-def get_week_game_data() -> str:
+
+def is_game_tomorrow(date):
+    return date == datetime.today().date() + timedelta(days=1)
+
+
+def get_paragraphs() -> list[str]:
     try:
         r = requests.get(URL)
         r.raise_for_status()
@@ -35,21 +43,37 @@ def get_week_game_data() -> str:
         r = None
 
     if r:
-        parser = BeautifulSoup(r.content, 'html.parser')
+        parser = BeautifulSoup(r.content, "html.parser")
 
-        paragraphs = [p.string for p in parser.find_all('p')]
+        paragraphs = [p.string for p in parser.find_all("p")]
 
-        res = ""
-        for p in paragraphs:
-            details = p.split()
-            if is_game_info(details) and is_date(details[1]):
-                date = datetime.strptime(details[1], '%d/%m/%Y').date()
+        return paragraphs
+
+    return []
+
+
+def get_next_day_game_data() -> str:
+    res = ""
+    for p in get_paragraphs():
+        details = p.split()
+        if is_game_info(details) and is_date(details[1]):
+            date = datetime.strptime(details[1], "%d/%m/%Y").date()
+            if is_game_tomorrow(date):
                 [_, date_str, time_str] = details
-                res += f"There is a game on {get_week_day_str(date)} the {date_str} at {time_str}\n"
+                res += f"There is a game tomorrow {get_week_day_str(date)} the {date_str} at {time_str}\n"
 
-        return res
-    
-    return ""
+
+def get_week_game_data() -> str:
+    res = ""
+    for p in get_paragraphs():
+        details = p.split()
+        if is_game_info(details) and is_date(details[1]):
+            date = datetime.strptime(details[1], "%d/%m/%Y").date()
+            [_, date_str, time_str] = details
+            res += f"There is a game on {get_week_day_str(date)} the {date_str} at {time_str}\n"
+
+    return res
+
 
 if __name__ == "__main__":
     get_week_game_data()
